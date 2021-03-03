4950028f2a11c0fd441e4d1374d3a56215e4563e
YCH-caihui <chyangbest@icloud.com>
2021-03-04 02:05:11 +0800

    addd

--- Java/src/main/java/linkedlist/SingleLinkedList.java
+++ Java/src/main/java/linkedlist/SingleLinkedList.java
@@ -1,17 +1,19 @@
 package linkedlist;
 
 class HeroNode {
+    int index;
     String name;
     HeroNode next;
 
-    HeroNode(String name) {
+    HeroNode(int index ,String name) {
         this.name = name;
+        this.index = index;
     }
 }
 
 public class SingleLinkedList {
 
-    private HeroNode mHeaderNode = new HeroNode("");
+    private HeroNode mHeaderNode = new HeroNode(-1,"");
 
 
     public void add(HeroNode node) {
@@ -23,10 +25,84 @@ public class SingleLinkedList {
         temp.next = node;
     }
 
+
+    public void addByOrder(HeroNode node) {
+        HeroNode temp = mHeaderNode;
+        boolean isCanAdd = true;
+        while (temp.next != null) {
+            if(temp.next.index > node.index) {
+                break;
+            } else if(temp.next.index == node.index) {
+                isCanAdd = false;
+                break;
+            }
+
+            temp = temp.next;
+        }
+        if(isCanAdd) {
+            node.next = temp.next;
+            temp.next = node;
+        } else {
+            System.out.println("相同的不能添加");
+        }
+
+    }
+
+
+    public void update(HeroNode heroNode) {
+        HeroNode temp = mHeaderNode;
+        boolean isFind = false;
+        while (temp != null) {
+            if(temp.index == heroNode.index) {
+                isFind = true;
+                break;
+            }
+            temp = temp.next;
+        }
+
+        if(isFind) {
+            temp.index = heroNode.index;
+            temp.name = heroNode.name;
+        } else {
+            System.out.println("没有找到:index"+heroNode.index+"     name:"+heroNode.name);
+        }
+    }
+
+    public void delete(int index) {
+        HeroNode temp = mHeaderNode;
+        boolean isFind = false;
+        while (temp.next != null) {
+            if(temp.next.index == index) {
+                isFind = true;
+                break;
+            }
+            temp = temp.next;
+        }
+
+        if(isFind) {
+            temp.next = temp.next.next;
+        } else {
+            System.out.println("没有找到:"+index);
+        }
+    }
+
+    public void reverse() {
+        HeroNode reverseList = new HeroNode(0,"");
+        HeroNode cur = mHeaderNode.next;
+        HeroNode next;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = reverseList.next;
+            reverseList.next = cur;
+            cur = next;
+        }
+        mHeaderNode = reverseList;
+    }
+
     public void dump() {
         HeroNode temp = mHeaderNode.next;
         while (temp != null) {
-            System.out.println(temp.name+"============");
+            System.out.println(temp.index+"============"+ temp.name);
             temp = temp.next;
         }
     }
@@ -37,9 +113,10 @@ public class SingleLinkedList {
  class SingleLinkedListDemo {
     public static void main(String[] args) {
         SingleLinkedList singleLinkedList = new SingleLinkedList();
-        singleLinkedList.add(new HeroNode("hahah"));
-        singleLinkedList.add(new HeroNode("heihei"));
-        singleLinkedList.add(new HeroNode("yes"));
+        singleLinkedList.add(new HeroNode(1,"hahah"));
+        singleLinkedList.add(new HeroNode(3,"heihei"));
+        singleLinkedList.add(new HeroNode(2,"yes"));
+        singleLinkedList.reverse();
         singleLinkedList.dump();
     }
 }
